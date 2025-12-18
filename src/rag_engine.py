d3fdcfbd4c6c05c4df0323ac506135b453ef2a8c
hamsa-joshua <joshuahamsaa@gmail.com>
2025-12-18 17:22:18 +0530

    rag engine code fix v1.0

--- src/rag_engine.py
+++ src/rag_engine.py
@@ -1,5 +1,5 @@
 import os
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 class RagEngine:
